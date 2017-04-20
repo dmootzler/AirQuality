2daf363c047ad35e2f0ab10aa5d23c4930b1abf5
Dan Metzler <dmetzler@ucsd.edu>
2017-04-20 02:58:53 -0700

    Fixed basic errors with parsing alexa input

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 
-var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = "amzn1.ask.skill.2d3b5f27-f479-4455-b710-7cb7c6da69e8";
 var SKILL_NAME = 'Air Quality';
 
-/**
- * Array containing space facts.
- */
+var calledEvent;
 
 exports.handler = function(event, context, callback) {
+    calledEvent = event;
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
@@ -17,12 +16,15 @@ exports.handler = function(event, context, callback) {
 
 var handlers = {
     'GetAirQualityIntent': function () {
+        // the city contained in the request
+        var city = calledEvent.request.intent.slots.CITY.value;
+        // TODO check if user has a favorite city stored. If not, begin intent to set city
+        
         // TODO make call to air quality API
-        var city = "Test";
+        
         // Create speech output
-        var speechOutput = "Here's your city: ";
-
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, city);
+        
+        this.emit(':tell', city);
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = "You can ask me about the air quality in a city, or, you can say exit... What can I help you with?";
